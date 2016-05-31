91aa43b8f3c432b046db096f20aa48ce5360f571
cpt2rah <cpt2rah@CTL-L10741.ctldomain.ad.local>
2016-05-31 14:46:01 +0530

    Rest Api Work Started with First Resource creation

---
+++ CouponsWorld/src/main/java/com/couponswworld/resources/OfferResource.java
@@ -0,0 +1,20 @@
+package com.couponswworld.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/offers")
+public class OfferResource {
+
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getOffers() {
+		return "{key:hello World}";
+	}
+	
+	
+	
+	
+}
